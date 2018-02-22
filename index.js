1562edafe052b5cd75451048207747da3b28e485
Panduro Calixto <e.panduro.calixto@accenture.com>
2018-02-22 05:00:54 -0300

    instalacion bode-parser y nodemon

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 'use strict'
 
 const express = require ('express')
+const bodyParser = require (body-parser)
 
 const app = express()
+const port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended:false}))
+app.use(bodyParser.json())
 
 app.listen(3000, ()=>{
     console.log('API REST corriendo en localhost:3000')
