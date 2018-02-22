3bb8c97de2c35c89f5096ac5b0bb9b29a7fad793
Panduro Calixto <e.panduro.calixto@accenture.com>
2018-02-22 05:23:46 -0300

    creacion get post put delete

--- index.js
+++ index.js
@@ -9,6 +9,27 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended:false}))
 app.use(bodyParser.json())
 
+app.get('/api/product',(req,red)=>{
+    res.status(200).send({products:[]})
+})
+
+app.get('/api/product/:producId',(req,res)=>{
+
+})
+
+app.post('/api/product',(req,res)=>{
+    console.log(req.body)
+    res.status(200).send({message: 'el producto se recibio'})
+})
+
+app.put('/api/product/:productId',(req,res)=>{
+
+})
+
+app.delete('/api/product/:productId',(req,res)=>{
+
+})
+
 app.listen(3000, ()=>{
     console.log('API REST corriendo en localhost:3000')
 })
