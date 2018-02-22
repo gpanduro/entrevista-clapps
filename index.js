8ad1b17a17c9bd22a35019ca2fcbfdeed153761e
Panduro Calixto <e.panduro.calixto@accenture.com>
2018-02-22 05:11:58 -0300

    arreglado problema  body-parser mal pasado godddddddddddd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const express = require ('express')
-const bodyParser = require (body-parser)
+const bodyParser = require ('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
